e834177dde8f8cc9e8a0aa6e37b17ca9815496c0
Mihkel Moorats <mihkel@sharecloud.ee>
2022-03-24 13:37:58 +0200

    final code after tutorial

---
+++ src/webparts/graphConsumer/GraphConsumerWebPart.ts
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneChoiceGroup 
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+
+import * as strings from 'GraphConsumerWebPartStrings';
+import GraphConsumer from './components/GraphConsumer';
+import { IGraphConsumerProps } from './components/IGraphConsumerProps';
+import { ClientMode } from "./components/ClientMode";
+
+export interface IGraphConsumerWebPartProps {
+  clientMode: ClientMode;
+}
+
+export default class GraphConsumerWebPart extends BaseClientSideWebPart<IGraphConsumerWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  protected onInit(): Promise<void> {
+    this._environmentMessage = this._getEnvironmentMessage();
+
+    return super.onInit();
+  }
+
+  public render(): void {
+    const element: React.ReactElement<IGraphConsumerProps > = React.createElement(
+      GraphConsumer,
+      {
+        clientMode: this.properties.clientMode,
+        context: this.context
+      }
+    );
+  
+    ReactDom.render(element, this.domElement);
+  }
+
+  private _getEnvironmentMessage(): string {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams
+      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    }
+
+    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
+    this.domElement.style.setProperty('--link', semanticColors.link);
+    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
+
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneChoiceGroup('clientMode', {
+                  label: strings.ClientModeLabel,
+                  options: [
+                    { key: ClientMode.aad, text: "AadHttpClient"},
+                    { key: ClientMode.graph, text: "MSGraphClient"},
+                  ]
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
